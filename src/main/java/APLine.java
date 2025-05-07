a72716aeffcb521329716c5fdc46ab2b8576cd05
MachineGunWilbur <19082019j@gmail.com>
2025-05-07 00:06:09 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,28 @@
 public class APLine
 {
-  /* your code here */
+  private double a,b,c;
+
+  public APLine(int x,int y,int z) {
+    a = x;
+    b = y;
+    c = z;
+
+  }
+
+  public double getSlope() {
+  double boom = (-a)/b;
+    return boom;
+
+
+  }
+
+  public boolean isOnLine(int xCord, int yCord) {
+  if(((a*xCord)+(b*yCord)+c) == 0)
+    return true;
+    else{
+    return false;
+    }
+  }
+
   
 }
